af8e7602811314c4e579560105398b79a14525da
joelkoehler <joel61499@gmail.com>
2020-10-23 11:47:41 -0600

    Added prototype UI to project

--- JuceProject/Source/PluginEditor.cpp
+++ JuceProject/Source/PluginEditor.cpp
@@ -26,7 +26,7 @@ NewProjectAudioProcessorEditor::NewProjectAudioProcessorEditor (VoltronAudioProc
 
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (900, 600);
 }
 
 NewProjectAudioProcessorEditor::~NewProjectAudioProcessorEditor()
@@ -38,6 +38,20 @@ void NewProjectAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    
+    juce::Image bgImage;
+    bgImage = juce::ImageCache::getFromMemory(BinaryData::nightsky_jpg, BinaryData::nightsky_jpgSize);
+    g.drawImage(bgImage, 0, 0, getWidth(), getHeight(), 0, 0, bgImage.getWidth(), bgImage.getHeight());
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawText("Noisier", 425, 500, 75, 20, juce::Justification::centredRight);
+    //g.drawFittedText ("Noisier", getLocalBounds(), juce::Justification::centred, 1);
+    g.setColour (juce::Colours::white);
+    // | (60) 150 (60) 150 (60) 150 (60) 150 (60) |
+    g.fillRect(60, 150, 150, 300);
+    g.fillRect(270, 150, 150, 300);
+    g.fillRect(490, 150, 150, 300);
+    g.fillRect(700, 150, 150, 300);
 }
 
 void NewProjectAudioProcessorEditor::resized()
